cc340451d3ff3ece6d283f5198b729e8240980be
Bence Szucs <bence.szucs200625@gmail.com>
2024-04-24 20:04:26 -0400

    Add project files.

---
+++ GamerBot/Program.cs
@@ -0,0 +1,45 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System.Web;
+
+namespace GamerBot
+{
+    public class Program
+    {
+        private static DiscordSocketClient _client;
+
+        public static Task Main() => new Program().MainAsync();
+
+        public async Task MainAsync()
+        {
+
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            DiscordSocketConfig socketConfig = new DiscordSocketConfig()
+            {
+                GatewayIntents = GatewayIntents.AllUnprivileged,
+                AlwaysDownloadUsers = true,
+
+            };
+
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            var token = config["Discord:Token"];
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
