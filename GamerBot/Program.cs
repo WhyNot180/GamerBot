c7f10ce1c92a7788834dd619fc20188c2dc2a8ee
Bence Szucs <bence.szucs200625@gmail.com>
2024-04-26 18:06:30 -0400

    Adds interaction handler to services

--- GamerBot/Program.cs
+++ GamerBot/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System.Web;
+using Discord.Interactions;
 
 namespace GamerBot
 {
@@ -14,14 +16,22 @@ namespace GamerBot
 
         public async Task MainAsync()
         {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
             using IHost host = Host.CreateDefaultBuilder()
                 .ConfigureServices((_, services) =>
                     services
+                    .AddSingleton(config)
                     .AddSingleton(X => new DiscordSocketClient(new DiscordSocketConfig
                     {
                         GatewayIntents = GatewayIntents.AllUnprivileged,
                         AlwaysDownloadUsers = true,
-                    })))
+                        UseInteractionSnowflakeDate = false,
+                    }))
+                    .AddSingleton(X => new InteractionService(X.GetRequiredService<DiscordSocketClient>()))
+                    .AddSingleton<InteractionHandler>()
+                 )
                 .Build();
 
             await RunAsync(host);
@@ -29,19 +39,29 @@ namespace GamerBot
 
         public async Task RunAsync(IHost host)
         {
-            IConfigurationRoot config = new ConfigurationBuilder()
-                .AddUserSecrets<Program>()
-                .Build();
 
             using IServiceScope serviceScope = host.Services.CreateScope();
             IServiceProvider serviceProvider = serviceScope.ServiceProvider;
 
             var _client = serviceProvider.GetRequiredService<DiscordSocketClient>();
+            var sCommands = serviceProvider.GetRequiredService<InteractionService>();
+            await serviceProvider.GetRequiredService<InteractionHandler>().InitializeAsync();
+            var config = serviceProvider.GetRequiredService<IConfigurationRoot>();
             
             _client.Log += Log;
+            sCommands.Log += Log;
+
             _client.Ready += async () =>
             {
                 Console.WriteLine("Bot Ready!");
+                bool success = ulong.TryParse(config["testGuild"], out ulong guildID);
+                if (success)
+                {
+                    await sCommands.RegisterCommandsToGuildAsync(guildID);
+                } else
+                {
+                    Console.WriteLine("Config parse failed to find guild!");
+                }
             };
 
             var token = config["Discord:Token"];
