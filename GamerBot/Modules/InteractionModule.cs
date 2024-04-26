7f1007b0754dd2c1c17edf74db9702eb94afa80a
Bence Szucs <bence.szucs200625@gmail.com>
2024-04-26 18:06:38 -0400

    Adds ping command

---
+++ GamerBot/Modules/InteractionModule.cs
@@ -0,0 +1,14 @@
+﻿using Discord;
+using Discord.Interactions;
+
+namespace GamerBot.Modules
+{
+    public class InteractionModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("ping", "Receive a ping in ms!")]
+        public async Task HandlePingCommand()
+        {
+            await RespondAsync($"PONG! ({Context.Client.Latency} ms)");
+        }
+    }
+}
